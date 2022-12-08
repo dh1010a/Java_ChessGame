dfa5f50b80f5085222daf2151a01252fd415f873
hojkim77 <rlaghwnd4534@gmail.com>
2022-12-08 14:18:48 +0900

    write button

--- src/BoardButton.java
+++ src/BoardButton.java
@@ -2,6 +2,27 @@ import java.awt.event.*;
 import javax.swing.*;
 
 public class BoardButton extends JButton implements ActionListener{
+	private ChessBoard board;
+	private ChessFrame frame;
+	private final int row;
+    private final int col;
+	public ChessButton(ChessBoard b,ChessFrame f, int row, int col) {
+		board = b;
+		frame = f;
+		this.row = row;
+	    this.col = col;
+		addActionListener(this); 
+	}
+	public int row() {
+		return row;
+	}
+	public int col() {
+		return col;
+	}
 	
+	public void actionPerformed(ActionEvent e) {
+		if (board.get_boardpeice(row, col).get_type != "")
+			board.get_road(int r, int c);
+	}
 	
 }
