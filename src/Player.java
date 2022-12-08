75ab2c9513a003cffe9050d28820dbdd38d4b327
dh1010a <=>
2022-12-08 13:34:42 +0900

    write Player

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,12 @@
 
 public class Player {
-
+	public int num;
+	private boolean turn;
+	private String name;
+	
+	public Player(String x, int n, boolean b){
+		name = x;
+		num = n;
+		turn = b;
+	}
 }
