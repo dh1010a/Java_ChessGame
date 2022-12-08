ab2840dccc71f6a4696a4e0921f02eb599a023d2
hojkim77 <rlaghwnd4534@gmail.com>
2022-12-08 13:36:38 +0900

    write boardpiece

--- src/BoardPiece.java
+++ src/BoardPiece.java
@@ -1,4 +1,16 @@
-
 public class BoardPiece {
-
+	int player;
+	String type;
+	
+	public BoardPiece(int p, String s) {
+		player = p;
+		type = s;
+	}
+	
+	public int get_playernum() {
+		return player;
+	}
+	public String get_type() {
+		return type;
+	}
 }
