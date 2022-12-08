6c7c515524628b603c67028076993ccaa163b1a7
hojkim77 <rlaghwnd4534@gmail.com>
2022-12-08 14:01:08 +0900

    write frame

--- src/ChessFrame.java
+++ src/ChessFrame.java
@@ -1,4 +1,11 @@
+import javax.swing.JFrame;
 
-public class ChessFrame {
-
+public class ChessFrame extends JFrame {
+	private ChessBoard board;
+	private BoardButton[][] button_board;
+	
+	public ChessFrame(ChessBoard b) {
+		board = b;
+		
+	}
 }
