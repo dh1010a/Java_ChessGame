e809f9c5c1e3131a1ba1b120ea74fd8937e6189f
hojkim77 <rlaghwnd4534@gmail.com>
2022-12-09 18:51:57 +0900

    color

--- src/BoardButton.java
+++ src/BoardButton.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import java.awt.event.*;
 import javax.swing.*;
+import java.awt.Graphics;
 
 public class BoardButton extends JButton implements ActionListener{
 	private ChessBoard board;
@@ -11,6 +13,7 @@ public class BoardButton extends JButton implements ActionListener{
 		frame = f;
 		this.row = row;
 	    this.col = col;
+	    this.setBackground(Color.blue);
 		addActionListener(this); 
 	}
 	public int row() {
@@ -20,6 +23,16 @@ public class BoardButton extends JButton implements ActionListener{
 		return col;
 	}
 	
+	/*public void setcolor(int r, int c){
+		if ((r + c) % 2 == 0){
+        	this.setBackground(Color.black);
+        	System.out.println("255");
+        }
+        else if ((r + c) % 2 == 1){
+        	this.setBackground(new Color(100, 0, 0));
+        	System.out.println("0");
+        }
+	}*/
 	public void actionPerformed(ActionEvent e) {
 		if (board.getBoardPiece(row, col).getType() != "")
 			board.calRoad(row, col);
--- src/ChessFrame.java
+++ src/ChessFrame.java
@@ -1,23 +1,39 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.WindowConstants;
-
 import java.awt.*;
-
+import javax.swing.ImageIcon;
 
 public class ChessFrame extends JFrame {
 	private ChessBoard board;
 	private BoardButton[][] button_board;
 	
 	public ChessFrame(ChessBoard b) {
+		ImageIcon img = new ImageIcon("./img/pngwing.png");
+		ImageIcon background = new ImageIcon("./img/chessboard.png");
+
 		board = b;
         button_board = new BoardButton[8][8];
         Container c = getContentPane();
         JPanel p = new JPanel(new GridLayout(8,8)); 
+        //p.drawImage(background, 0, 0, null);
         for (int row = 0; row < 8; row++) {
             for (int col = 0; col < 8; col++) {
-                button_board[row][col] = new BoardButton(board,this, row, col);
+                button_board[row][col] = new BoardButton(board,this, row, col); // 버튼에는 색이 안들어감,,
                 p.add(button_board[row][col]);
+                if ((row + col) % 2 == 0){
+                    //button_board[row][col].setIcon(img);
+                    button_board[row][col].setBackground(Color.black);
+
+                    //button_board[row][col].setOpaque(false);
+                    //button_board[row][col].setContentAreaFilled(false);
+
+                	System.out.println("255");
+                }
+                else if ((row + col) % 2 == 1){
+                	//button_board[row][col].setText("♚");
+                	System.out.println("0");
+                }
             }
     	}
         c.add(p,BorderLayout.CENTER);
